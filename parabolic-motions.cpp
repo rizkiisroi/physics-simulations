bddd3d4e0e3e2d2bc27a4ab1029360d1d200ba75
rizkiisroi <rizkiisroi@gmail.com>
2015-11-15 06:04:49 +0700

    Update of parabolic-motions.cpp
    I updated some comments so that you can understand the use of each lines, if you want to use this code, please write the author or type github.com/rizkiisroi at your footer

--- parabolic-motions.cpp
+++ parabolic-motions.cpp
@@ -1,8 +1,10 @@
 /*
-Alamsyah Rizki Isroi
-10211056
-Berikut merupakan program untuk menentukan posisi dari suatu benda
-dalam simulasi gerak parabola dari ketinggian 56 meter
+Alamsyah Rizki Isroi (rizkiisroi@live.com)
+Physics Department, Bandung Institute of Technology
+Here is C++ code to determine the position (time deppended) of a particle 
+in a parabolic motion.
+Compile	: g++ parabolic-motions.cpp -o parabolic
+Run	: ./parabolic
 */
 
 #include <fstream>
@@ -13,37 +15,35 @@ dalam simulasi gerak parabola dari ketinggian 56 meter
 #include <string.h>
 
 using namespace std;
-//inisialisasi beberapa konstanta dan kondisi awal
-const double h = 56; //tinggi maksimum
-const double g = 9.8; //konstanta gravitasi
-const double PI = 3.141529;
-const double teta = PI/4; //kondisi awal elevasi parabola
-const double v0 = (sqrt(2*g*h))/sin(teta);
-//fungsi posisi vertikal
+const double h = 56; //the maximum high of parabolic
+const double g = 9.8; //gravity constant
+const double PI = 3.141529; //phi constant
+const double teta = PI/4; //elevation angle of initial velocity
+const double v0 = (sqrt(2*g*h))/sin(teta); //initial velocity
+//function of vertical position
 double y(double t) {
 	double y0 = 0;
 	double f = y0 + v0 * sin(teta) * t - g * t * t / 2;
 	return f;
 }
-
-//fungsi posisi horizontal
+//function of horizontal position
 double x(double t) {
 	double v0x = v0 * cos(teta);
 	double x0 = 0;
 	return x0 + v0x * t;
 }
-
+//main function
 int main() {
 	double tmax = 2 * v0 * sin(teta) / g;
-	cout << "tmax adalah = " << tmax << endl;
+	cout << "tmax is = " << tmax << endl; //time to reach maximum high of the parabolic
 	ofstream fout;
-	fout.open("tugasparabola.txt");
+	fout.open("parabolic.txt"); //output will be printed in file parabolic.txt
 	if(fout.is_open()) {
-		fout << "#rx\try" << endl;
+		fout << "#rx\try" << endl; //header of the parabolic.txt
 		fout << fixed;
-		fout << setprecision(2); 
-		double t = 0.0;
-		double dt = 0.05;
+		fout << setprecision(2); //setting output to maximum 2 decimal place
+		double t = 0.0; //initializing time t
+		double dt = 0.05; //time different
 		while(t < tmax) {
 			double ry = y(t);
 			double rx = x(t);
